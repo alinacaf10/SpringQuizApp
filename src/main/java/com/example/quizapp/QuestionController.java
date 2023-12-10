21db86bbfe6888cf29b52444cbe7e1af27762ef9
Ali Nacafov <alinacaf10@gmail.com>
2023-12-11 02:12:22 +0400

    Create QuestionController

---
+++ src/main/java/com/example/quizapp/QuestionController.java
@@ -0,0 +1,15 @@
+package com.example.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("allQuestion")
+    public String getAllQuestion(){
+    return "These are all questions";
+    }
+}
